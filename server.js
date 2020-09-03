dd49781fafc05c184d1a8c7cafd9b33a74023295
Bhart <bhart.sharma99@gmail.com>
2020-09-03 15:49:20 +0530

    chore: add server js

--- server.js
+++ server.js
@@ -2,9 +2,9 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/spacex-launch-programs'));
 app.get('/*', function(req,res){
-res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+res.sendFile(path.join(__dirname, 'dist/spacex-launch-programs', 'index.html'))
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
